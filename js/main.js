dff56621188dca9a01c3fd3058f92e1ffba2a764
DeerMichel <mh74182@gmail.com>
2017-03-30 00:28:57 +0200

    Some browsers are simply NOT fun!

--- js/main.js
+++ js/main.js
@@ -2,25 +2,6 @@
 
 $(function() {
 
-  // setup party plugin
-  var rythm = new Rythm();
-  rythm.setMusic("/res/bounce.mp3");
-  rythm.addRythm("rythm-title", "pulse", 0, 5, { mix: 0.85, max: 1.15 });
-  rythm.addRythm("rythm-social", "twist", 100, 50, { mix: -25, max: 25 });
-
-  // party button click
-  $("h1 i.ion-play").click(function() {
-    $("h1 i.ion-play").removeClass("show");
-    $("i.ion-stop").addClass("show");
-    rythm.start();
-  });
-
-  // stop button click
-  $("i.ion-stop").click(function() {
-    $("i.ion-stop").removeClass("show");
-    rythm.stop();
-  });
-
   // sneakbar click -> scroll to about me
   $("div.sneakbar").click(function() {
     $("#about").velocity("stop").velocity("scroll", {duration: 500, easing: "easeOutCubic", offset: -15});
@@ -39,6 +20,25 @@ $(function() {
   // scroll listener
   $(window).scroll(function() {
     window.requestAnimationFrame(scrollHandler);
+  });
+
+  // setup party plugin (at end to prevent compatibility problems affecting the whole page experience)
+  var rythm = new Rythm();
+  rythm.setMusic("/res/bounce.mp3");
+  rythm.addRythm("rythm-title", "pulse", 0, 5, { mix: 0.85, max: 1.15 });
+  rythm.addRythm("rythm-social", "twist", 100, 50, { mix: -25, max: 25 });
+
+  // party button click
+  $("h1 i.ion-play").click(function() {
+    $("h1 i.ion-play").removeClass("show");
+    $("i.ion-stop").addClass("show");
+    rythm.start();
+  });
+
+  // stop button click
+  $("i.ion-stop").click(function() {
+    $("i.ion-stop").removeClass("show");
+    rythm.stop();
   });
 
   // a magic formula to reveal THE button
