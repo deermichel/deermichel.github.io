fef1eebcebc381eef10f0badc9dc15a1dc33881b
Micha Hanselmann <deermichel@github.com>
2019-07-15 22:14:47 -0700

    title styling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,4 +3,4 @@ require("../styles/index.scss")
 
 // set copyright year
 const footerYear = document.querySelector("footer .year")
-footerYear.textContent = new Date().getFullYear()
+// footerYear.textContent = new Date().getFullYear()
