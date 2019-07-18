a2d39b7d67e8412e02ca3175ebd2fc91d6ddbafe
Micha Hanselmann <deermichel@github.com>
2019-07-17 22:44:47 -0700

    cleaner css?

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,36 +5,26 @@ require("../styles/index.scss")
 const footerYear = document.querySelector("footer .year")
 // footerYear.textContent = new Date().getFullYear()
 
-const titleContainer = document.querySelector("main .title-container")
-const bgStripe = document.querySelector("main .bg .stripe")
-const projects = document.querySelectorAll("main .project")
-const showcaseHider = document.querySelector("main .showcase-hider")
-const bg = document.querySelector("main .bg")
+const header = document.querySelector("#header")
+const titleContainer = document.querySelector(".title-container")
 
 let skipFrame = false
 window.addEventListener("scroll", (e) => {
     const posY = window.scrollY
     if (!skipFrame) {
         window.requestAnimationFrame(() => {
-            // console.log(posY)
-            // titleContainer.style.opacity = 1 - Math.min(posY, 100) / 100.0
-            const progress = Math.min(96, posY) / 96.0
-            const rotation = 25.0 - 25.0 * progress;
-            titleContainer.style.transform = `translate(-50%, -${50 + 50 * progress}%)`
-            bgStripe.style.transform = `rotate(${rotation}deg)`
-            projects.forEach((project) => {
-                project.style.transform = `rotate(${rotation}deg)`
-            })
+            // const progress = Math.min(1, Math.max(1 - titleContainer.getBoundingClientRect().y / titleContainer.offsetTop, 0));
+            
+            // if (progress == 1) {
+            //     header.classList.add("fixed")
+            // } else {
+            //     // header.classList.remove("fixed")
+            // }
+            // if (posY < 260) {
+            //     header.classList.remove("fixed")
+            // }
 
-            if (posY > 96) {
-                titleContainer.classList.add("fixed")
-                showcaseHider.classList.add("fixed")
-                bg.classList.add("fixed")
-            } else {
-                titleContainer.classList.remove("fixed")
-                showcaseHider.classList.remove("fixed")
-                bg.classList.remove("fixed")
-            }
+            // console.log(progress)
 
             skipFrame = false
         })
