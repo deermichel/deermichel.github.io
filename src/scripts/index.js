c29e70574ff7582e0bb5d748d8fc50783b360fd0
Micha Hanselmann <deermichel@github.com>
2019-07-20 23:24:15 -0700

    getting somewhere

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,7 +6,7 @@ const footerYear = document.querySelector("footer .year")
 // footerYear.textContent = new Date().getFullYear()
 
 const stripe = document.querySelector("#header .stripe")
-const titleContainer = document.querySelector("#header .title-container")
+const headerContainer = document.querySelector("#header .header-container")
 const projects = document.querySelectorAll("#content .project")
 
 let skipFrame = false
@@ -18,14 +18,14 @@ window.addEventListener("scroll", (e) => {
 
             stripe.style.transform = `rotate(${25 * (1 - progress)}deg)`
             stripe.style.opacity = 1 - progress
-            header.style.height = `${Math.max(260, 460 - posY)}px`
+            header.style.height = `${460 - posY}px`
             projects.forEach((project) => project.style.transform = `rotate(${25 * (1 - progress)}deg)`)
 
-            // if (progress == 1) {
-            //     titleContainer.classList.add("fixed")
-            // } else {
-            //     titleContainer.classList.remove("fixed")
-            // }
+            if (progress == 1) {
+                headerContainer.classList.add("fixed")
+            } else {
+                headerContainer.classList.remove("fixed")
+            }
 
             skipFrame = false
         })
