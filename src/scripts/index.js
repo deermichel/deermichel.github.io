97a894c00c27c337fbd152d1d676b658712a48bb
Micha Hanselmann <deermichel@github.com>
2019-07-20 22:29:21 -0700

    its ok

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,8 +5,8 @@ require("../styles/index.scss")
 const footerYear = document.querySelector("footer .year")
 // footerYear.textContent = new Date().getFullYear()
 
-const bgStripe = document.querySelector("#header .bg-container .stripe")
-const header = document.querySelector("#header")
+const stripe = document.querySelector("#header .stripe")
+const titleContainer = document.querySelector("#header .title-container")
 const projects = document.querySelectorAll("#content .project")
 
 let skipFrame = false
@@ -16,10 +16,16 @@ window.addEventListener("scroll", (e) => {
         window.requestAnimationFrame(() => {
             const progress = Math.min(1, posY / 100)
 
-            bgStripe.style.transform = `rotate(${25 * (1 - progress)}deg)`
-            bgStripe.style.opacity = 1 - progress
+            stripe.style.transform = `rotate(${25 * (1 - progress)}deg)`
+            stripe.style.opacity = 1 - progress
             header.style.height = `${Math.max(260, 460 - posY)}px`
             projects.forEach((project) => project.style.transform = `rotate(${25 * (1 - progress)}deg)`)
+
+            // if (progress == 1) {
+            //     titleContainer.classList.add("fixed")
+            // } else {
+            //     titleContainer.classList.remove("fixed")
+            // }
 
             skipFrame = false
         })
