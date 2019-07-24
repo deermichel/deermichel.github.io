14cabf9a081ef9b01063340b0f2754a1639392ee
Micha Hanselmann <deermichel@github.com>
2019-07-23 22:26:47 -0700

    one idea

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,7 +7,6 @@ const footerYear = document.querySelector("footer .year")
 
 const stripe = document.querySelector("#header .stripe")
 const headerContainer = document.querySelector("#header .header-container")
-const projects = document.querySelectorAll("#content .project")
 
 let skipFrame = false
 window.addEventListener("scroll", (e) => {
@@ -19,8 +18,7 @@ window.addEventListener("scroll", (e) => {
             stripe.style.transform = `rotate(${25 * (1 - progress)}deg)`
             stripe.style.opacity = 1 - progress
             header.style.height = `${460 - posY}px`
-            projects.forEach((project) => project.style.transform = `rotate(${25 * (1 - progress)}deg)`)
-
+            
             if (progress == 1) {
                 headerContainer.classList.add("fixed")
             } else {
