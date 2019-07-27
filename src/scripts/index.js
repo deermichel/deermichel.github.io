5726dc4e5a0b24e2cb4b9b3b5dabf6e838443053
Micha Hanselmann <deermichel@github.com>
2019-07-26 19:40:35 -0700

    cleanup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,35 @@
-// load styless
+// load styles
 require("../styles/index.scss")
+require("@fortawesome/fontawesome-free/js/all")
 
 // set copyright year
-const footerYear = document.querySelector("footer .year")
+// const footerYear = document.querySelector("footer .year")
 // footerYear.textContent = new Date().getFullYear()
 
-const stripe = document.querySelector("#header .stripe")
-const headerContainer = document.querySelector("#header .header-container")
+// const stripe = document.querySelector("#header .stripe")
+// const headerContainer = document.querySelector("#header .header-container")
 
 let skipFrame = false
 window.addEventListener("scroll", (e) => {
     const posY = window.scrollY
     if (!skipFrame) {
         window.requestAnimationFrame(() => {
-            const progress = Math.min(1, posY / 100)
-
-            stripe.style.transform = `rotate(${25 * (1 - progress)}deg)`
-            stripe.style.opacity = 1 - progress
-            header.style.height = `${460 - posY}px`
             
-            if (progress == 1) {
-                headerContainer.classList.add("fixed")
-            } else {
-                headerContainer.classList.remove("fixed")
-            }
+            // const progress = Math.min(1, posY / 100)
+
+            // stripe.style.transform = `rotate(${25 * (1 - progress)}deg)`
+            // stripe.style.opacity = 1 - progress
+            // header.style.height = `${460 - posY}px`
+            
+            // if (progress == 1) {
+            //     headerContainer.classList.add("fixed")
+            // } else {
+            //     headerContainer.classList.remove("fixed")
+            // }
+
+            // header.style.filter = `blur(${posY / 80}px)`
+            header.style.opacity = 1 - posY / 260;
+            header.style.transform = `translate(0, -${posY / 3}px)`
 
             skipFrame = false
         })
