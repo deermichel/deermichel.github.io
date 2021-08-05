056fb0ab61e48fec33f980ac50671d3254835c81
Micha Hanselmann <micha.hanselmann@gmail.com>
2021-08-05 20:21:22 +0200

    almost done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,25 @@
 // load styles
 require("../styles/index.scss")
 
+// smooth scrolling polyfill
+const smoothscroll = require("smoothscroll-polyfill")
+smoothscroll.polyfill();
+
 // set copyright year
 const footerYear = document.querySelector("footer .year")
 footerYear.textContent = new Date().getFullYear()
+
+// mobile device
+const portfolioHeader = document.querySelector(".portfolio-header")
+const isMobile = /Android|iPhone|iPad|iPod|IEMobile|Opera Mini/i.test(navigator.userAgent);
+if (isMobile) {
+    // background attachment fallback
+    portfolioHeader.style.backgroundAttachment = "scroll"
+}
+
+// scroll to portfolio
+const portfolioLink = document.querySelector(".links-portfolio")
+portfolioLink.addEventListener('click', (e) => {
+    e.preventDefault()
+    portfolioHeader.scrollIntoView({ behavior: 'smooth' })
+})
