86cca789f4ec4d42895b0dc3e9f91237d81d48a6
Micha Hanselmann <micha.hanselmann@gmail.com>
2021-08-05 10:16:10 +0200

    add me section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,5 +2,5 @@
 require("../styles/index.scss")
 
 // set copyright year
-// const footerYear = document.querySelector("#footer .year")
-// footerYear.textContent = new Date().getFullYear()
+const footerYear = document.querySelector("footer .year")
+footerYear.textContent = new Date().getFullYear()
